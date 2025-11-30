9b11469974e78c19bc7b84b2f5214c61d1119e99
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-30 12:55:41 +0100

    feat: add common intarfaces file for resourses

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-console.log('Hello world')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
