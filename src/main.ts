7cd89112206868714a164b476e3d7441e7eb85b1
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-30 12:48:58 +0100

    init: start dev part 1 proces

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('Hello world')
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
